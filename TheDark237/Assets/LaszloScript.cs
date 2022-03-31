82a1411c5dbd99fa4a1721126d2bb82a4d9429bb
ges373 <gstrueby01@gmail.com>
2022-03-31 00:50:49 -0600

    Laszlo walks
    yay Laszlo walks

---
+++ TheDark237/Assets/LaszloScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaszloScript : MonoBehaviour
+{
+
+    public Rigidbody2D rBody;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rBody.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rBody.velocity.y);
+    }
+}
